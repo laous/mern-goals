f4235951e2091fc885d8d49562293779786a2e7a
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-03-20 13:10:36 +0100

    login and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
+        <Navbar />
         <Routes>
           <Route path="/">
             <Route path="register" element={<Register />} />
