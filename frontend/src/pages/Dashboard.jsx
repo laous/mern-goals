5f4f88acbf46531746f5e1ac4c94bb5ff703c1ea
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-03-20 23:49:31 +0100

    restrict dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) navigate("/login");
+  }, [user, navigate]);
   return <div>Dashboard</div>;
 };
 
